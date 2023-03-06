4644575ac34e1526990545688551ecddd665b0b9
Yerukondu Haritha <120496799+Haritha-7@users.noreply.github.com>
2023-03-06 21:31:45 +0530

    Day6 programs done

--- Day6/SPattern1.java
+++ Day6/SPattern1.java
@@ -18,6 +18,39 @@
  for(int i=1;i<=2;i++)
  System.out.print("*");
  System.out.println();
+ for(int j=1;j<=1;j++)
+ System.out.print("*");
+ for(int i=1;i<=1;i++)
+ System.out.print(" ");
+ System.out.print("*");
+ System.out.println();
+ for(int i=1;i<=3;i++)
+ System.out.print("*");
+ System.out.println();
+for(int j=1;j<=1;j++)
+ System.out.print("*");
+ for(int i=1;i<=1;i++)
+ System.out.print(" ");
+ System.out.print("*"); 
+ System.out.println();
+ for(int j=1;j<=1;j++)
+ System.out.print("*");
+ for(int i=1;i<=1;i++)
+ System.out.print(" ");
+ System.out.print("*");
+ System.out.println();
+ for(int j=1;j<=1;j++)
+ System.out.print("*");
+ for(int i=1;i<=1;i++)
+ System.out.print(" ");
+ System.out.print("*");
+ System.out.println();
+ for(int j=1;j<=1;j++)
+ System.out.print("*");
+ for(int i=1;i<=1;i++)
+ System.out.print(" ");
+ System.out.print("*");
+ 
  }
  }
  
