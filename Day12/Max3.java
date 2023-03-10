3422ca2351d8c18f0699f0eb62a9e87c014323de
Yerukondu Haritha <120496799+Haritha-7@users.noreply.github.com>
2023-03-10 07:24:56 +0530

    Day12 programs done

--- Day12/Max3.java
+++ Day12/Max3.java
@@ -15,12 +15,12 @@ max3=max2;
 max2=max1;
 max1=arr[i];
 }
-else if(arr[i]>max2)
+else if(arr[i]>max2 && arr[i]!=max1)
 {
 max3=max2;
 max2=arr[i];
 }
-else if(arr[i]>max3)
+else if(arr[i]>max3 && arr[i]!=max2)
 {
 max3=arr[i];
 }
