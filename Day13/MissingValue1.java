68511b28e4de4ff3ecaf559a935c94b86c1d0d44
Yerukondu Haritha <120496799+Haritha-7@users.noreply.github.com>
2023-03-12 07:52:57 +0530

    Day13 programs done

--- Day13/MissingValue1.java
+++ Day13/MissingValue1.java
@@ -3,11 +3,11 @@ public class MissingValue1
 {
 public static void main(String args[])
 {
-int n=7;
-int arr[]={2,7,4,3,1,6};
+int n=8;
+int arr[]={2,7,4,3,1,6,8};
 int t=n*(n+1)/2;
 int act=0;
-for(int i=0;i<n-1;i++)
+for(int i=0;i<arr.length;i++)
 act += arr[i];
 System.out.print(t-act);
 }
