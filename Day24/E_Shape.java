e85691421d92e28bcbb6fd606707f7c17bed6644
Yerukondu Haritha <120496799+Haritha-7@users.noreply.github.com>
2023-03-24 12:57:31 +0530

    Day24 programs done

---
+++ Day24/E_Shape.java
@@ -0,0 +1,28 @@
+//print E shape using arrays
+public class E_Shape
+{
+public static void main(String args[])
+{
+int arr[][]={
+     {3,7,9,0,1,2,6},
+     {1,2,5,7,3,0,3},
+     {3,6,8,6,1,6,1},
+     {2,5,1,7,3,9,5},
+     {2,5,8,0,2,4,3},
+     {4,5,8,0,2,5,1},
+     {5,1,4,6,8,0,3}
+     };
+     int n=arr.length;
+     for(int i=0;i<=n-1;i++)
+     {
+     for(int j=0,l=1;j<=n-1;j++,l+=2)
+     {
+     if(i==0||j==0||i==n-1||i==n/2)
+     System.out.print(arr[i][j]+" ");
+     else
+     System.out.print("  ");
+     }
+     System.out.println();
+     }
+     }
+     }
---
+++ Day24/Y_Shape.java
@@ -0,0 +1,28 @@
+//print y shape using arrays
+public class Y_Shape
+{
+public static void main(String args[])
+{
+int arr[][]={
+     {3,7,9,1,0,6,1},
+     {1,2,5,8,3,7,2},
+     {3,6,8,0,1,0,3},
+     {1,6,4,7,8,6,2},
+     {2,4,5,8,0,7,3},
+     {2,6,8,0,2,5,4},
+     {1,5,7,9,3,4,2}
+     };
+     int n=arr.length;
+     for(int i=0;i<=n-1;i++)
+     {
+     for(int j=0;j<=n-1;j++)
+     {
+     if(i==j&&i<=n/2||j==n-i-1&&i<=n/2||j==n/2&&i>n/2)
+     System.out.print(arr[i][j]+" ");
+     else
+     System.out.print("  ");
+     }
+     System.out.println();
+     }
+     }
+     }
