b5d531d8e17e370bacc7146c2b5ed62df21b222c
Yerukondu Haritha <120496799+Haritha-7@users.noreply.github.com>
2023-03-24 16:39:32 +0530

    Day24 programs done

---
+++ Day24/Shape_T.java
@@ -0,0 +1,29 @@
+//print T shape
+public class Shape_T
+{
+public static void main(String args[])
+{
+int arr[][]={
+    {2,8,5,6,9,1,0},
+    {1,3,5,7,9,0,2},
+    {4,5,7,9,0,2,5},
+    {1,3,4,6,8,9,3},
+    {1,3,5,7,9,0,2},
+    {5,7,8,9,2,1,5},
+    {2,7,5,4,0,8,1}
+    };
+    int n=arr.length;
+    for(int i=0;i<n;i++)
+    {
+    for(int j=0;j<n;j++)
+    {
+    if(j==n/2||i==0)
+    System.out.print(arr[i][j]+" ");
+    else
+    System.out.print("  ");
+    }
+    System.out.println();
+    }
+    }
+    }
+    
