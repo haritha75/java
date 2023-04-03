cd79381922a7fdc1e2e28055f53dac206f0e0654
Yerukondu Haritha <120496799+Haritha-7@users.noreply.github.com>
2023-04-03 14:16:55 +0530

    Day6 programs done

---
+++ Day6/X_Shape.java
@@ -0,0 +1,44 @@
+//print the bellow pattern
+
+/*
+
+\******/
+*\****/*
+**\**/**
+***\/***
+***/\***
+**/**\**
+*/****\*
+/******\
+
+
+*/
+
+
+public class X_Shape
+{
+public static void main(String args[])
+{
+int n=8;
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=n;j++)
+{
+if(i==j||i+j==n+1)
+{
+if(i+j==n+1)
+{
+System.out.print("/");
+}
+else
+{
+System.out.print("\\");
+}
+}
+else
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
