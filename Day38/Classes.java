454e29cf1c7600140bc360a10b97abc1f41f2b1b
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-04-08 13:52:39 +0530

    Day38 programs done

--- Day38/Classes.java
+++ Day38/Classes.java
@@ -6,19 +6,24 @@ public static void main(String args[])
 Car bmw=new Car();
 bmw.play(1);
 Car p=new Car();
-//p.price=40000;//it is not proper way
-//System.out.println(p.price);
+p.price=40000;//it is not proper way.it shows errors because price varible in private we not accesing .so you can use setters and getters method.
+System.out.println(p.price);
 p.setPrice(50000);//this one is good .calling function with object
 System.out.println(p.getPrice());
 Car c=new Car();
 c.setColor("red");//setting the function with object and then printing with getfunction.
 System.out.println(c.getColor());
+Car y=new Car();
+y.years(4);
+Car s=new Car();
+s.changeSpeed(false);
 }
 }
 class Car
 {
 //attributes or properties
 String color;
+private int years;
 private int price;// it is in private so not using other class.
 String company;
 String song[]={"dhoom","akasam","beast"};
@@ -34,6 +39,10 @@ void play(int id)
 {
 System.out.println(song[id]);
 return;
+}
+void years(int n)//actually years private but it working in another class because here we are declaring. and calling another class.here not using getters and setters method just declaring method and calling another method.
+{
+System.out.println("how many years it working");
 }
 //getters or setters
 int getPrice()
