b3d591919420b1f1c618273e33445d5df9dfe4a1
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-04-13 00:10:22 +0530

    Day43 programs done

---
+++ Day43/StaticInnerclass11.java
@@ -0,0 +1,20 @@
+public class StaticInnerclass11 {
+    public static void main(String[] args) {
+        Animal.Cat.sum();//it means in animal class and inner class function calling and dog function inherite into cat class.
+    }
+    
+}
+class Animal{
+    static int x;
+    static int sayHI(){
+        return 5;
+    }
+    static class Dog{
+        static void sum(){
+            System.out.println(5);
+        }
+    }
+    static class Cat extends Dog{// it means extends dog class so  dof function all inherite into cat class .
+
+    }
+}
