c5411deecc0a4f26464ebaee3e5b0576465ff431
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-04-13 18:00:55 +0530

    Day43 programs done

--- Day43/StaticInnerclass11.java
+++ Day43/StaticInnerclass11.java
@@ -1,6 +1,8 @@
 public class StaticInnerclass11 {
+   static int s;//instance variable working only with in the calss so use ststic if use other class or method.
     public static void main(String[] args) {
         Animal.Cat.sum();//it means in animal class and inner class function calling and dog function inherite into cat class.
+        System.out.println(s);
     }
     
 }
---
+++ Day43/Staticclass.java
@@ -0,0 +1,28 @@
+public class Staticclass {
+    static  int hello(){
+        System.out.println("good");
+        return 5;
+    }
+    public static void main(String[] args) {
+        hello();//here we calling 
+        
+        Animal.Dog dog=new Animal.Dog();
+        //System.out.println(dog);
+        dog.sum();
+        
+    }
+}
+class Animal{
+     static int x=9;
+     static int sayHello(){
+        System.out.println("Hi");
+        return 9;
+     }
+     static class Dog{//static inner class
+        int y=7;
+        void sum(){
+            System.out.println(y+x);//here we acceessing x value in inner class so mention static x variable.
+            sayHello();//here we are calling outer class function into inner class so mention static function because inner class function static.
+     }
+}
+}
