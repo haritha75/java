c89402d7ea956015be83bccea175b7462d523831
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-04-18 20:20:57 +0530

    Day48 programs done

--- Day48/Brute.java
+++ Day48/Brute.java
@@ -13,8 +13,8 @@ public class Brute {
 
         cells();  
         for(int i=0;i<a.length;i++) {
-            for(int j=0;i<a[0].length;j++){
-            System.out.println(a[i][j]+" ");
+            for(int j=0;j<a[0].length;j++){
+            System.out.print(a[i][j]+" ");
         }
         System.out.println();
     }
--- Day48/Brute1.java
+++ Day48/Brute1.java
@@ -64,7 +64,7 @@ public static boolean isMagicSquare() {
 static void display() {
     for(int i=0;i<a.length;i++){
         for(int j=0;j<a[0].length;j++) {
-            System.out.println(a[i][j]+" ");
+            System.out.print(a[i][j]+" ");
         }
         System.out.println();
     }
