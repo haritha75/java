5527a47a0726e795805cf7a7b058344252ca7146
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-04-20 18:38:28 +0530

    Day49 programs done

---
+++ Day49/Largest.java
@@ -0,0 +1,26 @@
+//find out largest value in large array.it is example brute force method
+
+public class Largest {
+    
+
+    public static void main(String[] args) {
+        int a[]={99,23,66,12,77,88,34,66,22,7,33,56,90,11};
+        int max=a[6];
+        int c=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]>max)
+            
+                c++;
+            max=a[i];
+            
+
+        }
+        System.out.println(max);
+
+        System.out.println(c);
+
+
+    }
+    
+
+}
