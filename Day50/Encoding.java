4b3e14eadc552c2ff7d8fa9d15993b963ce3d160
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-04-21 17:35:48 +0530

    Day50 programs done

---
+++ Day50/Encoding.java
@@ -0,0 +1,32 @@
+public class Encoding {
+    public static void main(String[] args) {
+
+        String str="jgnnq yqtnf";
+
+        char a[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+        for(int i=1;i<=25;i++) {
+
+            for(int j=0;j<str.length();j++) {
+
+            if(!(str.charAt(j)==' ')) {
+
+                int ind=str.charAt(j)-97-i;
+                int curNow=ind%26;
+                if(curNow<0)
+                curNow+=26;
+                System.out.print(a[curNow]);
+            }
+            else {
+
+            System.out.print(" ");
+        }
+
+    }
+        System.out.println("\n");
+    
+
+
+    }
+    }
+}
