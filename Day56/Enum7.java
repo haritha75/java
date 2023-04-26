4866bd990ff9899511c67c31e7af671e7936aefd
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-04-26 22:43:31 +0530

    Day56 programs done

---
+++ Day56/Enum7.java
@@ -0,0 +1,20 @@
+public class Enum7 {
+
+    public static void main(String[] args) {
+        
+        Brands b = Brands.ICIC;
+
+        System.out.println(b);
+    }
+    
+}
+enum Brands {
+
+    RELIANCE,
+    INFOSYS,
+    SBI,
+    ICIC,
+    HDFC,
+    TATA,
+    HUL
+};
