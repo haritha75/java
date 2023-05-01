fceaf13b551b44f2543a3fb486189df42143c371
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-05-01 23:03:35 +0530

    Day60 programs done

---
+++ Day60/TextBlocks.java
@@ -0,0 +1,23 @@
+//text blocks nothing but if you have same body type then use text blocks.denoted in three double quote characters.like this (""" """).
+
+public class TextBlocks {
+
+    public static void main(String[] args) {
+        
+        String str = """
+                haritha
+                ramya
+                jhansi
+                reddy
+                yashu
+                harika
+                keerthi
+                
+                """;
+                    
+             System.out.println(str);
+            
+       
+    }
+    
+}
---
+++ Day60/TextBlocks1.java
@@ -0,0 +1,31 @@
+    //text blocks nothing but if you have same body type then use text blocks.denoted in three double quote characters.like this (""" """).
+public class TextBlocks1 {
+
+
+    public static void main(String[] args) {
+        
+        String str = """
+               <html>
+               
+                <head>
+                
+                <title>html</title>
+                
+                  </head>
+                
+                   <body>
+                
+                      Hello guys!
+                    
+                    </body>
+                    
+                    </html>
+                
+                """;
+                    
+             System.out.println(str);
+            
+       
+    }
+    
+}
