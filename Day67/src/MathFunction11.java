df50ae0e622f0746bb003ac84631680a2a315a6c
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-05-09 07:41:52 +0530

    Day67 programs done

---
+++ Day67/src/MathFunction11.java
@@ -0,0 +1,29 @@
+public class MathFunction11 {
+
+    public static void main(String[] args) {
+        
+
+        double x =45.8;
+        double y = 3;
+        int z =54;
+
+        System.out.println(Math.abs(y));
+        System.out.println(Math.random());
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.min(x,y));
+        System.out.println(Math.sqrt(y));
+        System.out.println(Math.pow(x, y));
+        System.out.println(Math.log(y));
+        System.out.println(Math.log10(y));
+        System.out.println(Math.exp(y));
+        System.out.println(Math.cbrt(z));
+        System.out.println(Math.ceil(x));
+        System.out.println(Math.floor(x)); 
+
+        System.out.println(Math.sin(y));
+        System.out.println(Math.cos(y));
+        System.out.println(Math.tan(y));
+        System.out.println(Math.sinh(x));
+    }
+    
+}
