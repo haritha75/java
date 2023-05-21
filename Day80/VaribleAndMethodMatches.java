edfd0cc330edb28629a473e038af7c11d81720bb
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-05-21 12:12:00 +0530

    Day80 programs done

--- Day80/VaribleAndMethodMatches.java
+++ Day80/VaribleAndMethodMatches.java
@@ -1,10 +1,35 @@
 import java.util.regex.Pattern;
+import java.util.Scanner;
 
 public class VaribleAndMethodMatches {
 
     public static void main(String[] args) {
-        boolean p = Pattern.matches("[^\\dA-Z]{1}[a-zA-Z0-9$_\\S]", "varibleDeclaration");
-        System.out.println(p);
+
+        Scanner sc = new Scanner(System.in); 
+
+        boolean res1 = Pattern.matches("[^\\dA-Z]{1}[a-zA-z0-9_$\\S]*", "variableDeclaration");
+        System.out.println(res1);
+
+        String  regExp = "[^\\dA-Z@#!%^&*()]{1}[a-zA-Z0-9$_\\S]*"; //[^\\dA-z&!()^*]{1}[\\w\\S$]*
+
+        boolean res;
+        do {
+
+            System.out.println("enter  varible name");
+            String input = sc.next();
+            res = Pattern.matches("regExp", "input");
+            if(res) 
+            
+               System.out.println("your entered correct varible name");
+           else 
+          
+              System.out.println("enter valid varible name");
+
+
+
+
+        }while(!res);
+
     }
     
 }
