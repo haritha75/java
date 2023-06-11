996947c53c127152b1797b4e6e04c89576f3e933
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-06-11 11:48:44 +0530

    Day99 programs done

--- Day99/geometricjavadoc/package-search-index.js
+++ Day99/geometricjavadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"rectanguarjavadoc"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"geometricjavadoc"}]
