72534c159c11b1bfcc9b113a464dc18f80dde14c
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-06-24 08:49:25 +0530

    Day109 programs done

---
+++ Day109/Garbage_CollectorExample.java
@@ -0,0 +1,38 @@
+//gorbage collector worked for whenever the reference variable not pointing to the object then it removed that  object from the heap.it automatically happening in java but not like that in c language.
+public class Garbage_CollectorExample {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("23","svu");
+
+        Student s2 = new Student("1","jntu");
+        Student s3 = new Student("4","svce"); //here s3 pointing to the object.
+        s3 = s2; //whenever s3 assigning s2 that time s3 pointing tothe s2 object then previous object of s3 will be removed automatically from the heap.
+
+        System.out.println(s3);
+        
+    }
+}
+
+class Student  {
+
+    String rollNO ;
+    String collegeName;
+
+    Student () {
+
+    }
+    Student(String rollNO,String collegeName) {
+
+        this.rollNO = rollNO;
+        this.collegeName = collegeName;
+    }
+    void hello () {
+        System.out.println(this.rollNO);
+        System.out.println(this.collegeName);
+    }
+    @Override
+    public String toString() {
+        return "Student [rollNO=" + rollNO + ", collegeName=" + collegeName + "]";
+    }
+}
