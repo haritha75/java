b0faae54fb84f3393135bbd32b6803d77e3c9e3e
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-07-14 00:02:42 +0530

    Day124 programs done

---
+++ Day124/EmployeeMain.java
@@ -0,0 +1,14 @@
+import employee.Employee;
+
+public class EmployeeMain {
+
+    public static void main(String[] args) {
+        
+              Employee employee = new Employee();
+
+              employee.setEmployeeNo("Ha-9754");
+              employee.setCompensation(45000);
+              employee.display();
+    }
+    
+}
