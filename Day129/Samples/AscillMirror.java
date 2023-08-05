6b6be593949c7d56597347c2f5f4d9814372c448
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-08-05 16:03:13 +0530

    Day129 programs done

---
+++ Day129/Samples/AscillMirror.java
@@ -0,0 +1,15 @@
+public class AscillMirror {
+    public static void main(String[] args) {
+        String cow = """
+                                    _______\s
+                                   < hello >
+                                    -------\s
+                            ^__^   /       \s
+                    _______/(oo)  /        \s
+                /\\/(       /(__)           \s
+                   | w----||               \s
+                   ||     ||               \s                  
+                """;
+        System.out.println(cow);
+    }
+}
