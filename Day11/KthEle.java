14405f2944c5c63506366d2fc0542eb0ca7c23fc
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-09-21 22:42:09 +0530

    Day11 work done

--- Day11/KthEle.java
+++ Day11/KthEle.java
@@ -7,7 +7,7 @@ public static void main(String args[])
 int arr[]={4,7,2,66,35,8};
 Arrays.sort(arr);
 int k=1;
-System.out.println(arr[k+3]);
+System.out.println(arr[k+3]+" ");
 System.out.print(arr[k+1]);
 }
 }
