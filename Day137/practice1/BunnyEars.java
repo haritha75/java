c74e6c00862b2e6f9dd576f1057b23f9b5104c77
Yerukondu Haritha <yerukonduharitha@gmail.com>
2024-01-25 21:14:42 +0530

    Day137 work done

---
+++ Day137/practice1/BunnyEars.java
@@ -0,0 +1,18 @@
+public class BunnyEars {
+  
+  public static void main(String[] args) {
+
+    System.out.println(bunnyEars(4));
+    
+  }
+  int bunnyEars(int bunnies) {
+
+    if(bunnies==0)
+      return 0;
+    if(bunnies==1)
+      return 2;
+    return 2+bunnyEars(bunnies-1);
+  }
+  
+  
+}
