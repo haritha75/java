8d2f0a9960951f1b0dc28370be82ef7d713d940a
Yerukondu Haritha <yerukonduharitha@gmail.com>
2024-03-05 09:04:45 +0530

    Day137 work done

---
+++ Day137/practice1/ReverseWordsInString.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reverseWords(String s) {
+
+        s = s.trim().replaceAll(" \\s+", " ");
+        String[] words = s.split(" ");
+        StringBuilder reversedString = new StringBuilder();
+        for(int i=words.length -1;i>=0;i--){
+            reversedString.append(words[i]).append(" ");
+        }
+        return reversedString.toString().trim();
+    }
+}
